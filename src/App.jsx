61bc1d6fdbe833ab0063a06e48dd7ec7c044c91b
Lishan-ahemd <ahmedlishan17@gmail.com>
2023-09-12 23:13:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Header></Header>
+      <div className='md:flex max-w-5xl mx-auto'>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
